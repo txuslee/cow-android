32873750f5c71ee56de2d028581966936e155f9b
Jesus Liarte Gabriel <jliarte@blitsoftware.com>
2015-03-28 17:24:28 +0100

    Project setup

---
+++ gradle/debug-key.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+
+keytool -genkey -v -keystore debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000 -dname "CN=Android Debug,O=Android,C=US"
